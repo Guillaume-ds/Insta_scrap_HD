324288fca8fc2c36c1dfd68a1cb662ce616be70a
Guillaume-ds <guillaume.de-surville@hec.edu>
2023-07-10 18:55:58 +0200

    requirements

--- Utils/Insta_Scrapper.py
+++ Utils/Insta_Scrapper.py
@@ -23,7 +23,7 @@ class instaScrapper():
     """
 
     def __init__(self):
-        @st.st.cache_resource 
+        @st.cache_resource 
         def installff():
             os.system('sbase install geckodriver')
             os.system('ln -s /home/appuser/venv/lib/python3.7/site-packages/seleniumbase/drivers/geckodriver /home/appuser/venv/bin/geckodriver')
